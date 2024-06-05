9b45613e427e3d7607721d0eb91f55cec491d11e
Leap-29 <j05huayau.29@gmail.com>
2024-06-05 02:30:21 +0100

    main frame and panel

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,38 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class TicTacToe {
+    int boardWidth = 500;
+    int boardHeight = 500;
+    
+    JFrame frame = new JFrame("Tic Tac Toe");
+    JLabel textLabel = new JLabel();
+    JPanel topPanel = new JPanel();
+    JPanel boardPanel = new JPanel();    
+
+    TicTacToe() {
+        frame.setSize(boardWidth, boardHeight);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        topPanel.setLayout(new BorderLayout());    
+        topPanel.add(textLabel);
+        frame.add(topPanel, BorderLayout.NORTH);
+
+        textLabel.setText("Tic Tac Toe");
+        textLabel.setFont(new Font(null, Font.BOLD, 50));
+        textLabel.setBackground(Color.DARK_GRAY);
+        textLabel.setForeground(Color.WHITE);
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setOpaque(true);
+        
+        boardPanel.setLayout(new GridLayout());
+        boardPanel.setBackground(Color.DARK_GRAY);
+        frame.add(boardPanel);
+        
+    }
 
 }
